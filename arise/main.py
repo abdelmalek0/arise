1253c9d4c194191d13c658a92beeed00fe84266f
abdelmalek0 <djamaa.abdelmalek98@gmail.com>
2024-10-16 21:14:28 +0100

    Changed colors used

--- arise/main.py
+++ arise/main.py
@@ -16,7 +16,13 @@ from arise.config import ENV_FOLDER
 console = Console()
 visibility_options = ["public", "private"]
 python_versions = get_python_versions()
-text_color = "#6ab0de"
+COLORS = {
+    "primary": "#FF6F61",
+    "secondary": "#9E9E9E",
+    "success": "#28a745",
+    "failure": "#8B0000",
+    "form": "#00FFFF",
+}
 
 load_dotenv(dotenv_path=os.path.join(ENV_FOLDER, ".env"))
 
@@ -36,35 +42,35 @@ def main():
 def run():
     project = dict()
 
-    console.print(Markdown("# Welcome To Python Project Initializer"))
+    console.print(Markdown("# **Welcome To Arise**", style=COLORS["primary"]))
     if not os.getenv("GITHUB_ACCESS_TOKEN") or not os.getenv("GITHUB_USERNAME"):
         console.print(Markdown("**You must set up your GitHub credentials first.**"))
         exit()
 
-    console.print(Markdown("### Project Setup"))
+    console.print(Markdown("### Project Setup", style=COLORS["secondary"]))
     # Get the name of the project
     while not project.get("name", None):
-        console.print("name: ", style=text_color, end="")
+        console.print("name: ", style=COLORS["form"], end="")
         project["name"] = input()
 
     # Get the description of the project
-    console.print("description: ", style=text_color, end="")
+    console.print("description: ", style=COLORS["form"], end="")
     project["description"] = input()
 
     # Get the visibility of the project
-    console.print("visibility:", style=text_color)
+    console.print("visibility:", style=COLORS["form"])
     visibility_index = TerminalMenu(visibility_options).show()
     console.print(visibility_options[visibility_index])
     project["private"] = True if visibility_index == 1 else False
 
     # Get the Python version to use
-    console.print("python version:", style=text_color)
+    console.print("python version:", style=COLORS["form"])
     python_index = TerminalMenu(list(zip(*python_versions))[0]).show()
     console.print(list(zip(*python_versions))[0][python_index])
     project["python"] = python_versions[python_index]
 
     # Get the Path of the project
-    console.print("path: ", style=text_color, end="")
+    console.print("path: ", style=COLORS["form"], end="")
     project["path"] = os.path.abspath(input())
     # Choose the directory of projects
     change_directory(
@@ -74,11 +80,10 @@ def run():
     progress = Progress()
     progress.start()
 
-    console.print(Markdown("\n\n\n### Project Creation"))
+    console.print(Markdown("\n\n\n### Project Creation", style=COLORS["secondary"]))
 
     # Create a poetry project
     create_poetry_project(progress, project)
-
     # Init git and commit the first changes
     github_utils.init_git(progress)
     # Create a github repo and push project to it
@@ -93,32 +98,32 @@ def login():
     credentials = dict()
     tries_count = 0
 
-    console.print(Markdown("# Welcome To Python Project Initializer"))
-    console.print(Markdown("### Github Setup"))
+    console.print(Markdown("# **Welcome To Arise**", style=COLORS["primary"]))
+    console.print(Markdown("### Github Setup", style=COLORS["secondary"]))
 
     while (not credentials.get("GITHUB_USERNAME", None)) and tries_count <= TRIES_LIMIT:
         tries_count += 1
-        console.print("username: ", style=text_color, end="")
+        console.print("username: ", style=COLORS["form"], end="")
         credentials["GITHUB_USERNAME"] = input()
 
     while (
         not credentials.get("GITHUB_ACCESS_TOKEN", None) and tries_count <= TRIES_LIMIT
     ):
         tries_count += 1
-        console.print("access token: ", style=text_color, end="")
+        console.print("access token: ", style=COLORS["form"], end="")
         credentials["GITHUB_ACCESS_TOKEN"] = input()
 
     if not credentials.get("GITHUB_ACCESS_TOKEN") or not credentials.get(
         "GITHUB_USERNAME"
     ):
-        console.print("Error: ", end="", style="#8B0000")
+        console.print("Error: ", end="", style=COLORS["failure"])
         print("Missing GitHub credentials.")
     else:
         if not github_utils.save_github_credentials(credentials):
             console.print("Error: ", end="", style="#8B0000")
             print("Saving credentials went wrong.")
 
-        console.print("Success: ", end="", style="#28a745")
+        console.print("Success: ", end="", style=COLORS["success"])
         print("Credentials saved.")
 
 
